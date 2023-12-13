c77f53cd8f2093d1ce5aab735293d51bc0b808d8
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-12-13 17:57:37 +0000

    fix: update imports

--- src/app/genres/[id]/page.tsx
+++ src/app/genres/[id]/page.tsx
@@ -1,11 +1,7 @@
 import 'server-only'
 
-import { prisma } from '@/lib/db'
 import { GenreForm } from '@/components/genre-form'
-import { saveGenre } from '@/server/save-genre'
-import { Genre } from '@prisma/client'
-import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
+import { prisma } from '@/lib/db'
 
 type Props = {
   params: {
